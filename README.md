5373768527d64ba470b7410c0c37218f692e511f
Pig Monkey <pm@pig-monkey.com>
2017-05-13 11:56:33 -0700

    initial import
    imported from my prezto fork

---
+++ README.md
@@ -0,0 +1,12 @@
+# notes.sh
+
+Shortcuts for easily managing text-based notes.
+
+See: https://pig-monkey.com/2012/12/notes-unix/
+
+## Configuration
+
+The variable `$NOTEDIR` defines the location of the note directory. It should
+be exported in the shell environment. 
+
+    export NOTEDIR=~/documents/notes
---
+++ notes.sh
@@ -0,0 +1,75 @@
+# Attempt to append the appropriate extension to a filename, preferring gpg
+# over txt.
+buildfile() {
+    # If an extension was given, use it.
+    if [[ "$1" == *.* ]]; then
+        echo "$1"
+    # If no extension was given...
+    else
+        # ... try the file without any extension.
+        if [ -e "$1" ]; then
+            echo "$1"
+        # ... try the file with a gpg extension.
+        elif [ -e "$1".gpg ]; then
+            echo "$1".gpg
+        # ... use a txt extension.
+        else
+            echo "$1".txt
+        fi
+    fi
+}
+
+# Verify the note directory is defined and exists.
+init() {
+    if [ -z "$NOTEDIR" ]; then
+        echo '$NOTEDIR is undefined.'
+        return 2
+    fi
+    if [ ! -d "$NOTEDIR" ]; then
+        mkdir -p "$NOTEDIR"
+    fi
+}
+
+# Create or edit notes.
+n() {
+    if init; then
+        # If no note was given, list the notes.
+        if [ -z "$1" ]; then
+            ls -lhtr --color=always "$NOTEDIR"
+        # If a note was given, open it.
+        else
+            $EDITOR "$(buildfile "$NOTEDIR"/"$1")"
+        fi
+    else
+        return $?
+    fi
+}
+
+# Find a note by title.
+nf() {
+    if init; then
+        if [ -z "$1" ]; then
+            ls -lhtr --color=always "$NOTEDIR"
+        else
+            ls -lhtr --color=always "$NOTEDIR" | grep -i --color=always "$1"
+        fi
+    else
+        return $?
+    fi
+}
+
+# Search within notes.
+ns() {
+    if init; then
+        cd "$NOTEDIR"
+        grep -rin --color=always "$1"
+        cd "$OLDPWD"
+    else
+        return $?
+    fi
+}
+
+# Set zsh autocompletion for notes.
+if [ -n "$ZSH_NAME" ]; then
+    compctl -W "$NOTEDIR" -f n
+fi
